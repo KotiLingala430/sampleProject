337c854e8e602fc01d30966a058d1b29d9ca76c3
ACER <ACER@LAPTOP-VUF6S1B0>
2022-11-21 19:41:57 +0530

    second

--- NewProject/src/FbLgin.java
+++ NewProject/src/FbLgin.java
@@ -1,8 +1,16 @@
 
 public class FbLgin {
+	static WebDriver driver;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:/Users/ACER/eclipse-workspace/demo1/drivers/chromedriver_win32 (8)/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("email")).sendKeys("kotilingala15@gmail.com");
+        driver.findElement(By.id("pass")).sendKeys("pass1234");
+        Thread.sleep(5000);
+        driver.close();
 
 	}
 
