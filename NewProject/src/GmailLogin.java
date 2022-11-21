bac0241dd8537bb3a3d7ec249a0ed5e0be0f51be
ACER <ACER@LAPTOP-VUF6S1B0>
2022-11-21 19:59:20 +0530

    21-11-2022 :Created Fblgin and GmailLogin pages

--- NewProject/src/GmailLogin.java
+++ NewProject/src/GmailLogin.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FbLgin {
+public class GmailLogin {
 	static WebDriver driver;
 
 	public static void main(String[] args) throws Exception {
@@ -14,7 +14,6 @@ public class FbLgin {
         driver.findElement(By.id("pass")).sendKeys("pass1234");
         Thread.sleep(5000);
         driver.close();
-
 	}
 
 }
