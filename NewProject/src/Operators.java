f084af95a6f97e061e273f99e4c48b914c7f5c11
koti <ACER@LAPTOP-VUF6S1B0>
2022-11-22 10:10:15 +0530

    22-11-2022:Created Java Operators file

---
+++ NewProject/src/Operators.java
@@ -0,0 +1,43 @@
+
+public class Operators {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println("Arithmatic Operators");
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		System.out.print("................................");
+		
+		System.out.println("Relational Operators");
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a<=b);
+		System.out.println(a>=b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+		System.out.println("..............................");
+		
+		
+		boolean x=true;
+		boolean y=false;
+		System.out.println("Logical Operators");
+		System.out.println(x && y);
+		System.out.println(x||y);
+		System.out.println("..............................");
+		
+		int c;
+		c=a+b;
+		System.out.println("Assignment Operators");
+		System.out.println(c);
+		c++;
+		System.out.println(c);
+		c--;
+		System.out.println(c);
+
+	}
+
+}
